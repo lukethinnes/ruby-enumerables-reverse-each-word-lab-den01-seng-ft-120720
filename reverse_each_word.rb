fbfb5e75d57dccbb6140f817bf64491a5e705db0
Luke Thinnes <lukethinnes@yahoo.com>
2020-11-24 21:39:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
   string = string.split(" ")
-  newString = string.collect do |str| 
+  newString = string.collect do |str|
       str.reverse
     end
  return newString.join(' ')
